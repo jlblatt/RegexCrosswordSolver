7979b259363fd5bf970a17e6d57045a74309d280
Jason Blatt <jason@jasonblatt.net>
2015-12-24 19:52:35 -0500

    Brute force approach

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RegexCrosswordSolver
 A javascript solver for [regexcrossword.com](regexcrossword.com)
+
+Currently only works via brute-force until I have more time to experiment.  This means that it is *slow*!
--- rcs.js
+++ rcs.js
@@ -2,10 +2,9 @@
 
   var clues, solution, p = [0, 0];
   var startTime = new Date(), endTime, elapsedTime;
-  
+
   var chars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-  var wildcard = "~";
-  
+
   function init()
   {
     jQuery.noConflict();
@@ -13,7 +12,9 @@
     // hexagonal puzzle
     if(jQuery("form.puzzle-hexagonal").length > 0)
     {
-      // lol later
+      clues = [
+        // lol later
+      ];
     }
 
     // rectangular puzzle
@@ -32,51 +33,13 @@
       dims.push(clues[i].length);
       for(var j = 0; j < clues[i].length; j++)
       {
-        clues[i][j] = {
-          string: clues[i][j].getAttribute('title')
-          regex: new RegExp(clues[i][j].getAttribute('title'))
-        }
+        clues[i][j] = new RegExp(clues[i][j].getAttribute('title'));
       }
     }
 
     // setup solution array
     solution = createArray.apply(this, dims);
-
-    // prune the number of characters we need to check
-    for(var i = 0; i < solution.length; i++)
-    {
-      var xclue = clues[0][i].string;
-      var xcount = clues[1].length;
-
-      for(var j = 0; j < solution[i].length; j++)
-      {
-        var yclue = clues[1][j].string;
-        var ycount = clues[0].length;
-
-        solution[i][j] = chars;
-
-        for(var k = 0; k < solution[i][j].length; k++)
-        {
-          // setup the strings/regexes to test against
-          var xstring = '';
-          for(var x = 0; x < xcount; x++)
-          {
-            if(x == j) xstring += solution[i][j][k];
-            else xstring += wildcard;
-          }
-
-          var ystring = '';
-          for(var y = 0; y < ycount; y++)
-          {
-            if(y == 1) ystring += solution[i][j][k];
-            else ystring += wildcard;
-          }
-
-        }
-      }
-    }
-
-    console.log(solution);
+    setInitialArrayState(solution);
 
     //solve();
 
@@ -86,28 +49,7 @@
   {
     var solved = false;
 
-    // check for win condition - 2 possible ways to do this:
-    //
-    // 1) make sure only one character remains in every element of solution[]
-    //      -- quicker, but fails if there is more than one valid solution to a puzzle
-    //
-    // 2) run all regexs against the current state of solution[]
-    //      -- expensive, but works if there is more than one valid solution to a puzzle
-    //
-    // a final version of the algorithm should implement both in some manner
-    // for now, use 1) and assume each puzzle only has one solution
-    
-    check:
-    for(var i = 0; i < solution.length; i++)
-    {
-      for(var j = 0; j < solution[i].length; j++)
-      {
-        if(solution[i][j].length > 1)
-        {
-          break check;
-        }
-      }
-    }
+    // check for win condition
 
     if(solved)
     {
@@ -121,14 +63,14 @@
 
     for(var i = 0; i < solution[p[0], p[1]].length; i++)
     {
-      //eliminate more letters if possible
+
     }
 
     solve();
   }
 
   // http://stackoverflow.com/a/966938
-  function createArray(length) 
+  function createArray(length)
   {
     var arr = new Array(length || 0), i = length;
     if(arguments.length > 1)
@@ -137,6 +79,16 @@
       while(i--) arr[length-1 - i] = createArray.apply(this, args);
     }
     return arr;
+  }
+
+  // http://stackoverflow.com/a/15854485/3365483
+  function zeroArray(arr)
+  {
+    for(var i = 0; i < arr.length; i++)
+    {
+      if(Array.isArray(arr[i])) zeroArray(arr[i]);
+      else arr[i] = 0;
+    }
   }
 
   var s = document.createElement("script");
